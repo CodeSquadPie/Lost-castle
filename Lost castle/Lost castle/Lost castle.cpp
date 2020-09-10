2acd793da50f504ae44a7d19a738f91fe2ca0f3e
WARsageR <warsager109@gmail.com>
2020-09-11 02:24:17 +0300

    сделал чтение данных из xml и на его основе рендер, хотя там много временного, надо больше данных из xml брать

--- Lost castle/Lost castle/Lost castle.cpp
+++ Lost castle/Lost castle/Lost castle.cpp
@@ -5,21 +5,26 @@
 using namespace  sf;
 using namespace tinyxml2;
 
-
 const int RESOLUTION_X = 800;
 const int RESOLUTION_Y = 600;
 const float ACCELERATION_QUOTIENT = 0.1f;
 const float SLOWDOWN_QUOTIENT = 1.006f;
 const float FALL_ACCELERATION_QUOTIENT = 0.2f;
 
+RenderWindow window(sf::VideoMode(RESOLUTION_X, RESOLUTION_Y), "SFML works!");
+
+int *current_map;
 Clock timer;
 Vector2f view_center(0.f,0.f);
 Vector2f view_size((float)RESOLUTION_X/2.f,(float)RESOLUTION_Y/2.f);
-unsigned int current_map[];
+Texture tile_map_image;
+Texture hero_texture;
 
+Sprite background_brush;
 int tile_size_x;
 int tile_size_y;
-
+int map_size_x;
+int map_size_y;
 float hero_dx = 0;
 float hero_dy = 0;
 float hero_dtx = 0;
@@ -27,9 +32,20 @@ float hero_dty = 0;
 float hero_max_walking_speed = 200.f;
 float hero_max_speed = 800.f;
 
+
+
+void render_tile_brute_force(int x, int y, int type);
+
 char map_file[] = "../assets/maps/debug/debug_map.tmx";
 
-void viewed_tiles(Vector2f *view_center, Vector2f *view_size, int tile_size_x, int tile_size_y)
+
+
+int querry_tile_on_map(int x,int y)
+{
+    return current_map[y*map_size_x+x];
+}
+
+void viewed_tiles(Vector2f *view_center, Vector2f *view_size)
 {
     float tile_size_x_f = (float)tile_size_x;
     float half_size_x = view_size->x/2;
@@ -38,9 +54,20 @@ void viewed_tiles(Vector2f *view_center, Vector2f *view_size, int tile_size_x, i
     Vector2f down_right_corner(view_center->x + half_size_x, view_center->y + half_size_y);
     int x1 = (int)up_left_corner.x/tile_size_x;
     int y1 = (int)up_left_corner.y/tile_size_y;
-    int x2 = (int)down_right_corner.x / tile_size_x;;
-    int y2 = (int)down_right_corner.y / tile_size_y;;
-
+    int x2 = (int)down_right_corner.x / tile_size_x;
+    int y2 = (int)down_right_corner.y / tile_size_y;
+    int temp_int;
+    for(int i=y1;i<y2;i++)
+    {
+        if(i<0){i=0;}
+        for(int j=x1;j<x2;j++)
+        {
+            if(j<0){j=0;}
+            temp_int = querry_tile_on_map(j,i);
+            render_tile_brute_force(j,i,temp_int);
+            //std::cout << "X: " << j << " Y : " << i << " Type: " << temp_int << std::endl;
+        }
+    }
 }
 
 void load_map_tileset(char *file_location)
@@ -62,30 +89,58 @@ void load_map_tileset(char *file_location)
     XMLElement *image_info = tileset_info->FirstChildElement();
     const char *tileset_image_location = image_info->Attribute("source");
 
+    
 }
 
 int load_map(char *file_location)
 {
-    int map_width_tiles;
-    int map_height_tiles;
-    int tilewidth;
-    int tileheight;
-
     XMLDocument map_file;
     map_file.LoadFile(file_location);
     
     XMLElement *map_info = map_file.FirstChildElement();
-    map_info->QueryIntAttribute("width",&map_width_tiles);
-    map_info->QueryIntAttribute("height",&map_height_tiles);
-    map_info->QueryIntAttribute("tilewidth", &tilewidth);
-    map_info->QueryIntAttribute("tileheight", &tileheight);
+    map_info->QueryIntAttribute("width",&map_size_x);
+    map_info->QueryIntAttribute("height",&map_size_y);
+    map_info->QueryIntAttribute("tilewidth", &tile_size_x);
+    map_info->QueryIntAttribute("tileheight", &tile_size_y);
 
-    XMLElement *tileset = map_info->FirstChildElement();
+    XMLElement *tileset_info = map_info->FirstChildElement();
 
-    std::cout << map_width_tiles << std::endl;
-    std::cout << map_height_tiles << std::endl;
+    XMLElement *layer_info = tileset_info->NextSiblingElement();
+    XMLElement *data_info = layer_info->FirstChildElement();
+
+    char *map_string = (char*)data_info->GetText();
+    char *str_context = NULL;
+    int *map = new int[map_size_x*map_size_y];
+    char *char_current;
+    int number;
+    char_current = strtok_s(map_string, ",", &str_context);
+    int i = 0;
+    while (char_current != NULL)
+    {
+        sscanf_s(char_current, "%d", &number);
+        char_current = strtok_s(NULL, ",", &str_context);
+        map[i] = number;
+        i++;
+    }
+    delete current_map;
+    current_map = map;
+    //std::cout << number << std::endl;
+    //std::cout << map[54]+map[53] << std::endl;
+    //std::cout << map_width_tiles << std::endl;
+    //std::cout << map_height_tiles << std::endl;
     return map_file.ErrorID();
 }
+
+void render_tile_brute_force(int x, int y, int type)
+{
+    int type_coord_x = type*tile_size_x-tile_size_x;
+    background_brush.setTexture(tile_map_image);//TODO: proper indexing system based on metadata of tileset xml
+    background_brush.setTextureRect(IntRect(type_coord_x, 0, tile_size_x, tile_size_y));
+    background_brush.setPosition((float)x*tile_size_x,(float)y*tile_size_y);
+    window.draw(background_brush);
+}
+
+
 
 int main()
 {
@@ -95,17 +150,20 @@ int main()
     View view;
     view.setCenter(Vector2f(0.f,0.f));
     view.setSize(Vector2f((float)RESOLUTION_X/2.f, (float)RESOLUTION_Y/2.f));
-
-    RenderWindow window(sf::VideoMode(RESOLUTION_X, RESOLUTION_Y), "SFML works!");
-    window.setView(view);
-    Texture map;
-    Texture hero_texture;
     hero_texture.loadFromFile("../assets/characters/hero/adventurer-v1.5-Sheet.png");
+    tile_map_image.loadFromFile("../assets/maps/debug/debug_room_tileset.jpg");
+    
+    window.setView(view);
+    
+
     Sprite hero_sprite;
     hero_sprite.setTexture(hero_texture);
     hero_sprite.setTextureRect(IntRect(0, 0, 50, 37));
 
     timer.restart();
+
+    //std::cout << querry_tile_on_map(10,7) <<std::endl; // must be 2
+    
 
 
     while (window.isOpen())
@@ -134,11 +192,10 @@ int main()
         Time time_elapsed = timer.restart();
         hero_dtx = time_elapsed.asSeconds() * hero_dx;
         hero_dty = time_elapsed.asSeconds() * hero_dy;
-
         //TODO: correction of dtx and dty by collision
-
         hero_sprite.move(hero_dtx,hero_dty);
         window.clear();
+        viewed_tiles(&view_center, &view_size);
         window.draw(hero_sprite);
         window.display();
     }
--- Lost castle/assets/maps/debug/debug_room_tileset.tsx
+++ Lost castle/assets/maps/debug/debug_room_tileset.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="debug room tileset"="32" tileheight="32" tilecount="3" columns="3">
+<tileset version="1.4" tiledversion="1.4.2" name="debug room tileset" tilewidth="32" tileheight="32" tilecount="3" columns="3">
  <image source="debug_room_tileset.jpg" width="96" height="32"/>
 </tileset>
